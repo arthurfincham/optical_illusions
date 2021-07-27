62b4455afba7306e7d63ec75a95b0a2d7c370f37
ARTHUR FINCHAM <arthurfincham@me.com>
2021-07-27 15:35:44 +0100

    Cafe Wall is done, needs to be offset or cropped

--- cafe_wall/sketch.js
+++ cafe_wall/sketch.js
@@ -6,13 +6,13 @@ function setup() {
 
 function draw() {
   background(255);
-  x_jump = 42;
-  y_jump = 42;
+  x_jump = 55;
+  y_jump = 55;
   stroke('grey');
   strokeWeight(3);
 
-    for (var j = 0 ; j < 3; j++) {
-      for (var i = 0; i < 15; i++) {
+    for (var j = 0 ; j < 50; j++) {
+      for (var i = 0; i < 50; i++) {
         if (j % 2 == 0) {
           fill(0);
         } else {
@@ -22,7 +22,13 @@ function draw() {
         } else {
           fill(255);
         };
-        rect(i * x_jump + 12 + j*10, j * y_jump, 47, 47);
+        if (j % 3 == 0){
+          rect(i * x_jump + 12 + 30, j * y_jump, 60, 60);
+        } else if (j % 2 == 0){
+          rect(i * x_jump + 12 + 15, j * y_jump, 60, 60);
+        } else {
+          rect(i * x_jump + 12, j * y_jump, 60, 60);
+        }
       }
     }
 
