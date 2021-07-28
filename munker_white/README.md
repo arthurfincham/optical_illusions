f784b39d1e72a034476a3635d96df9eff28649a8
ARTHUR FINCHAM <arthurfincham@me.com>
2021-07-28 12:08:51 +0100

    Munker White completed

---
+++ munker_white/README.md
@@ -0,0 +1,10 @@
+<h1>Munker White Illusion</h1>
+
+The illusion relies on the hue of the foreground stripes, not the colors in the background behind the spheres. So, if you remove the crisscrossing stripes, the illusion disappears, leaving only identical beige balls.
+
+*taken from [livescience.com](https://www.livescience.com/confetti-munker-white-optical-illusion.html)*
+
+[Preview](https://editor.p5js.org/arthurfincham/sketches/DEx72t7kt) of my p5.js rendition!
+
+<h2>Original Illusion</h2>
+<img src="../resources/munker-white.jpeg" width="300" height="300">
--- munker_white/sketch.js
+++ munker_white/sketch.js
@@ -4,6 +4,42 @@ function setup() {
 
 function draw() {
   background(255);
+ 
 
-  ellipse(width/2, height/2, 200, 200)
+
+  // LINES
+  noStroke();
+  for (var i = 0; i < 50; i++){
+    if (i % 2 == 0) {
+      fill('magenta');
+      rect(0, 750/50 * i, width)
+  } else {
+    fill('green');
+      rect(0, 750/50 * i, width)
+  }
+}
+
+
+
+noStroke();
+  for (var i = 0; i < 50; i++){
+    if (i % 2 == 0) {
+      fill('magenta');
+      rect(width*0.2, 750/50 * i, width*0.2)
+  } else {
+    fill(255, 238, 138);
+      rect(width*0.2, 750/50 * i, width*0.2)
+  }
+}
+
+noStroke();
+  for (var i = 0; i < 50; i++){
+    if (i % 2 == 0) {
+      fill(255, 238, 138);
+      rect(width*0.6, 750/50 * i, width*0.2)
+  } else {
+    fill('green');
+      rect(width*0.6, 750/50 * i, width*0.2)
+  }
+}
 }
